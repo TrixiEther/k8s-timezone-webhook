aee32be8a812ff37e3301161de78ad4b13da2f20
TrixiEther <trixiether@gmail.com>
2020-05-28 12:06:28 +0300

    Dummy commit, just add webhook code

--- src/hello.go
+++ src/hello.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func Hello(text string) string {
 	return text
 }
 
 func main() {
-	fmt.Printf(Hello("Hello, world.\n"))
+    log.Println("Starting webhook server ...")
+
+    mux := http.NewServeMux()
+    mux.HandleFunc("/mutateTimezone", handleMutateTimezone)
+
+    s := &http.Server{
+        Addr:           "8080",
+        Handler:        mux,
+        ReadTimeout:    10 * time.Second,
+        WriteTimeout:   10 * time.Second,
+        MaxHeaderBytes: 1 << 20, // 1048576
+    }
+
 }
